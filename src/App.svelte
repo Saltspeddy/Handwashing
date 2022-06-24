9412fb10c52d72612abe5ca18a5c96da2f6bd0f8
Saltspeddy <walkercoco30@gmail.com>
2022-06-24 22:24:47 +0300

    third commit

--- src/App.svelte
+++ src/App.svelte
@@ -1,1 +1,20 @@
-<h1>Test</h1>
+<script>
+	import Timer from "./Timer.svelte";
+	import HowTo from "./HowTo.svelte";
+</script>
+
+<style>
+	h1, 
+	h3{
+		text-align: center;
+	}
+</style>
+<h1>Handwashing app</h1>
+
+<Timer />
+<HowTo />
+
+<h3>
+	<a href=""> Picture source</a>
+	<a href=""> Sound source</a>
+</h3>
---
+++ src/ProgressBar.svelte
@@ -0,0 +1,21 @@
+<style>
+    .progress-container{
+        margin: 20px 0;
+        border:solid #b7b7b7 1px;
+    }
+
+    .progress-bar{
+        height: 30px;
+        background-color: rgb(33, 187, 33);
+    }
+</style>
+
+<div bp="grid">
+    <div bp="offset-5@md 4@md 12@sm" 
+    class="progress-container">
+        <div class="progress-bar"
+            style="width: 20%">
+            <span class="sr-only">%</span>
+        </div>
+    </div>
+</div>
